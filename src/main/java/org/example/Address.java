583392ab0d48b378571f3232fb7cc8f7aaa2180c
2583626 <2583626@edu.vaniercollege.qc.ca>
2025-12-13 00:22:14 -0500

    Added Lombok imports for Getters, Setters, AllArgsConstructor and NoArgsConstructor. Also created the basis for public class Address and isPostalCodeValid method

--- src/main/java/org/example/Address.java
+++ src/main/java/org/example/Address.java
@@ -1,7 +1,41 @@
 package org.example;
 
-public class Address {
-    static void main() {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    private int StreetNo;
+    private String street;
+    private String city;
+    private Province province;
+    private String postalCode;
+
+    public static boolean isPostalCodeValid(String postalCode) {
+        if (postalCode == null || postalCode.length() != 6) {
+            return false;
+        }
+
+        for (int i = 0; i < 6; i++) {
+            char c = postalCode.charAt(i);
+
+            if (i % 2 == 0) {
+                if (!Character.isLetter(c)) {
+                    return false;
+                }
+            } else {
+                if (!Character.isDigit(c)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
