80ccb33c08099bf64c131789727fe3ed52645f7f
2583626 <2583626@edu.vaniercollege.qc.ca>
2025-12-13 17:57:20 -0500

    Created class Student and added the fields. Also added the useful lombok utilities.

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,4 +1,21 @@
 package org.example;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Student {
-}
+
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private ArrayList<Course> registeredCourses;
+
+    private static int nextId = 1;
