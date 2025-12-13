8b63ff821f142c27a6c147888096bea07ccad583
2583626 <2583626@edu.vaniercollege.qc.ca>
2025-12-13 01:22:32 -0500

    Added my lombok imports for the class Department.java and added my fields.

--- src/main/java/org/example/Department.java
+++ src/main/java/org/example/Department.java
@@ -1,4 +1,16 @@
 package org.example;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.EqualsAndHashCode;
+
+@Getter
+@ToString
+@EqualsAndHashCode
 public class Department {
+
+    private String departmentId;
+    @Setter private String departmentName;
+    private static int nextId = 1;
 }
