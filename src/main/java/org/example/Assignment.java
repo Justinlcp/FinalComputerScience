390b46762f780b4a5f87a13f7141846786e8432b
2583626 <2583626@edu.vaniercollege.qc.ca>
2025-12-13 20:53:04 -0500

    Added method void generateRandomScore()

--- src/main/java/org/example/Assignment.java
+++ src/main/java/org/example/Assignment.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 @Getter
 @Setter
@@ -15,3 +16,27 @@ public class Assignment {
     private ArrayList<Integer> scores;
 
     private static int nextId = 1;
+
+    public void generateRandomScore() {
+        Random rand = new Random();
+
+        for (int i = 0; i < scores.size(); i++) {
+            int randomNum = rand.nextInt(11);
+            int score;
+
+            if (randomNum == 0) {
+                score = rand.nextInt(60);
+            } else if (randomNum == 1 || randomNum == 2) {
+                score = 60 + rand.nextInt(10);
+            } else if (randomNum == 3 || randomNum == 4) {
+                score = 70 + rand.nextInt(10);
+            } else if (randomNum >= 5 && randomNum <= 8) {
+                score = 80 + rand.nextInt(10);
+            } else {
+                score = 90 + rand.nextInt(11);
+            }
+
+            scores.set(i, score);
+        }
+    }
+}
