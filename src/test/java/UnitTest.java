9da97e01aae230ce34dcd30b4efacacaf0530baa
2583626 <2583626@edu.vaniercollege.qc.ca>
2025-12-14 16:20:22 -0500

    Changed name to UnitTest and added testPostalCode

---
+++ src/test/java/UnitTest.java
@@ -0,0 +1,34 @@
+import org.example.Address;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Assertions;
+
+public class UnitTest {
+
+    @Test
+    @DisplayName("A1B2C3 --> True")
+    void testPostalCode1() {
+        String code = "A1B2C3";
+        boolean result = true;
+        boolean output = Address.isPostalCodeValid(code);
+        Assertions.assertEquals(result, output);
+    }
+
+    @Test
+    @DisplayName("123456 --> False")
+    void testPostalCode2() {
+        String code = "123456";
+        boolean result = false;
+        boolean output = Address.isPostalCodeValid(code);
+        Assertions.assertEquals(result, output);
+    }
+
+    @Test
+    @DisplayName("ABCDEF -- False")
+    void testPostalCode3() {
+        String code = "ABCDEF";
+        boolean result = false;
+        boolean output = Address.isPostalCodeValid(code);
+        Assertions.assertEquals(result, output);
+    }
+}
