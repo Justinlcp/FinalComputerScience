8bbd20f96b115ac489b7c9722c73810e5ae5d8d1
2583626 <2583626@edu.vaniercollege.qc.ca>
2025-12-14 00:45:58 -0500

    Made the method toTitleCase with the specifics requirements

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -1,4 +1,22 @@
 package util;
 
 public class Util {
+    public static String toTitleCase(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+
+        String[] words = str.split(" ");
+        String result = "";
+
+        for (String word : words) {
+            if (word.length() > 0) {
+                result += word.substring(0, 1).toUpperCase()
+                        + word.substring(1).toLowerCase()
+                        + " ";
+            }
+        }
+
+        return result.trim();
+    }
 }
