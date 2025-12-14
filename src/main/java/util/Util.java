50da14d53b8acef3a2997236b4439a8d8cbd4d87
2583626 <2583626@edu.vaniercollege.qc.ca>
2025-12-14 00:49:09 -0500

    Added the description for the method

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -1,5 +1,11 @@
 package util;
 
+/**
+ * Converts each word in a string to title case. Assume words in str are always
+ * separated by space, such as Yi Wang, Computer Science And Math. str.split() method
+ * will be used. All studentName, departmentName, courseName stored in a class needs
+ * to be in title case.
+ */
 public class Util {
     public static String toTitleCase(String str) {
         if (str == null || str.isEmpty()) {
