a5b4e94dc4ed8d5349e8411e5a084136b0c7af3e
2583626 <2583626@edu.vaniercollege.qc.ca>
2025-12-13 23:26:01 -0500

    chose to modify name for better clarity

--- src/main/java/org/example/Course.java
+++ src/main/java/org/example/Course.java
@@ -63,12 +63,12 @@ public class Course {
     }
 
     public boolean addAssignment(String assignmentName, double weight, int maxScore) {
-        Assignment assignment = new Assignment(assignmentName, weight);
+        Assignment newAssignment = new Assignment(assignmentName, weight);
 
         for (int i = 0; i < registeredStudents.size(); i++) {
-            assignment.getScores().add(null);
+            newAssignment.getScores().add(null);
         }
 
-        assignments.add(assignment);
+        assignments.add(newAssignment);
         return true;
     }
