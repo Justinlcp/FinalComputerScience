7737f21c67db4da0c8d7114d397940808e29ba75
2583626 <2583626@edu.vaniercollege.qc.ca>
2025-12-13 20:38:06 -0500

    Created the class Assignment.java and added the fields. Also added Lombok utilities.

---
+++ src/main/java/org/example/Assignment.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@Getter
+@Setter
+public class Assignment {
+
+    private String assignmentId;
+    private String assignmentName;
+    private double weight;
+    private ArrayList<Integer> scores;
+
+    private static int nextId = 1;
