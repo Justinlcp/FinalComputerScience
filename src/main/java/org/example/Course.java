71ba827d58d18ec8637582b4f32aafff5f8d6db4
2583626 <2583626@edu.vaniercollege.qc.ca>
2025-12-13 21:34:52 -0500

    Created Course.java, added lombok utilities and fields

---
+++ src/main/java/org/example/Course.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Course {
+
+    private String courseId;
+    private String courseName;
+    private double credits;
+    private Department department;
+    private ArrayList<Assignment> assignments;
+    private ArrayList<Student> registeredStudents;
+
+    private static int nextId = 1;
